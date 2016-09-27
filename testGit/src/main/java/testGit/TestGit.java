bdd2b3a521fcb593a68c5d447e60302cbce21daa
Yann CHENU <chenu.y@sfeir.com>
2016-09-27 17:23:45 +0200

    conflits

--- testGit/src/main/java/testGit/TestGit.java
+++ testGit/src/main/java/testGit/TestGit.java
@@ -4,11 +4,8 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-<<<<<<< HEAD
 		System.out.println("Hello Git!");
 		System.out.println("Hello World!");
-=======
->>>>>>> origin/branchTest
 		System.out.println("Hello World!");
 	}
 }
