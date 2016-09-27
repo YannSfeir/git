c0278399ba139a7b2af7bdc7397e146ec2b58a25
Yann CHENU <chenu.y@sfeir.com>
2016-09-27 17:02:49 +0200

    ajout de world

--- testGit/src/main/java/testGit/TestGit.java
+++ testGit/src/main/java/testGit/TestGit.java
@@ -5,5 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Git!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
